adda2a6da0540713558515289fd4b736bcb54c71
wasifdawar <m.wasifdawar@gmail.com>
2021-12-20 01:34:17 +0500

    Start one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      My Chat app
+      <h1>My Chat app</h1>
     </div>
   );
 }

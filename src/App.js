35be7b4ca81386ba53bca133931aa474bcc7247b
wasifdawar <m.wasifdawar@gmail.com>
2021-12-21 00:27:41 +0500

    Feat: Mirror

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import firebase from "firebase/app";
+import "firebase/firebase-firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore;
 
 function App() {
   return (
     <div className="App">
       <h1>My Chat app</h1>
+      <h2>Firebase is not working</h2>
     </div>
   );
 }
